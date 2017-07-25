a33cad8701b6af819eb567e5c96df0521584fe39
pf90 <paidifitz2@gmail.com>
2017-07-25 15:58:02 +0100

    Changes from the first ionic course, intro course.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  myArray = ['This', 'is', 'my', 'first', 'ngFor'];
 
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
