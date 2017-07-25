c9caef40d05b7853b26465988750aa7198322743
pf90 <paidifitz2@gmail.com>
2017-07-25 16:24:10 +0100

    Course 2: using Cordova plugins, accessing the camera.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  myArray = ['This', 'is', 'my', 'first', 'ngFor'];
+  myImage: string;
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  captureImage() {
+    let options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      correctOrientation: true
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myImage = imageData;
+    }, err => {
+      console.log('error: ', err);
+    });
   }
 
 
